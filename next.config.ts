7267bc6afb54b60f2bdea614b71abd0a23facb38
PredatorXxRIPxX <wassimou009@gmail.com>
2025-02-13 15:57:25 +0100

    fixing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 const nextConfig = {
-  distDir: 'out',
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
-  basePath: '/MPPS', // Replace with your GitHub repo name
-  assetPrefix: '/MPPS/', // Ensure assets are loaded correctly
 };
 
 module.exports = nextConfig;
