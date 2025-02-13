d56108f1777494cdf32c814b36ee5a56870b748d
PredatorXxRIPxX <wassimou009@gmail.com>
2025-02-13 16:07:03 +0100

    fixing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output : 'export',
-  images: {
-    unoptimized: false,
-  },
+  images: { unoptimized: false }, 
 };
 
 module.exports = nextConfig;
