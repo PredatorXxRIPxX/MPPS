b561babf76b65c35e8952133635d81b60d25b3af
PredatorXxRIPxX <wassimou009@gmail.com>
2025-02-13 15:52:11 +0100

    fixing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'MPPS'; 
-
-module.exports = {
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/MPPS', // Replace with your GitHub repo name
+  assetPrefix: '/MPPS/', // Ensure assets are loaded correctly
 };
+
+module.exports = nextConfig;
